d04f316e70f3eb7112fc8d4977eec4042decb857
PUNEETH UMESH BHARADWAJ <puneeth.u.bharadwaj@outlook.com>
2016-08-17 15:00:22 -0500

    Injecting Objects
    Dependency injection. Triangle object has 3 Point objects. Spring looks
    at XML and creates Point objects for Triangle object, before passing the
    Triangle object to the DrawingApp

--- SpringDemo04/src/org/puneeth/springframework/Triangle.java
+++ SpringDemo04/src/org/puneeth/springframework/Triangle.java
@@ -2,31 +2,37 @@ package org.puneeth.springframework;
 
 public class Triangle {
 
-	private String type;
-	private int height;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	public Triangle(String type) {
-		this.type = type;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public Triangle(int height) {
-		this.height = height;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
+	public Point getPointB() {
+		return pointB;
 	}
 
-	public String getType() {
-		return type;
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
 	}
 
-	public int getHeight() {
-		return height;
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		System.out.println(getType() + " Triangle drawn of height " + getHeight());
+		System.out.println("Point A = (" + getPointA().getX() + "," + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + "," + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + "," + getPointC().getY() + ")");
 	}
 }
