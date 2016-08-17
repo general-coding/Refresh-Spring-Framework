a2c44dd7659e4152efc29b86dc81eebc29ae9cde
PUNEETH UMESH BHARADWAJ <puneeth.u.bharadwaj@outlook.com>
2016-08-17 11:30:44 -0500

    Spring will convert values to constructors, when defined in XML. However, beware of overloaded constructors.

---
+++ SpringDemo3/src/org/puneeth/springframework/Triangle.java
@@ -0,0 +1,28 @@
+package org.puneeth.springframework;
+
+public class Triangle {
+
+	private String type;
+	private int height;
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+
+	public String getType() { 
+		return type;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void draw() {
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
+	}
+}
